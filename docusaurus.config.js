c09bd20a54bb453b593a8217583453c2e8931043
tison <wander4096@gmail.com>
2022-06-06 13:02:28 +0800

    more project specific staff
    Signed-off-by: tison <wander4096@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,25 +6,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Apache Kvrocks (Incubating)',
+  tagline: 'Apache Kvrocks is a distributed key value NoSQL database that uses RocksDB as storage engine and is compatible with Redis protocol.',
+  url: 'https://kvrocks.apache.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'apache',
+  projectName: 'incubator-kvrocks-website',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zh-CN'],
+    localeConfigs: {
+        'zh-CN': {label: "简体中文"},
+      },
   },
 
   presets: [
@@ -33,18 +31,16 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          path: 'docs',
+          routeBasePath: "/docs",
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/apache/incubator-kvrocks-website/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/apache/incubator-kvrocks-website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -57,23 +53,36 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Apache Kvrocks',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'apache-kvrocks',
           src: 'img/logo.svg',
         },
         items: [
+          {type: 'doc', docId: 'supported-commands', position: 'right', label: 'Docs'},
+          {to: '/blog', label: 'Blog', position: 'right'},
           {
-            type: 'doc',
-            docId: 'supported-commands',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            type: 'dropdown',
+            label: 'ASF',
             position: 'right',
+            items: [
+              {label: 'Foundation', to: 'https://www.apache.org/'},
+              {label: 'License', to: 'https://www.apache.org/licenses/'},
+              {label: 'Events', to: 'https://www.apache.org/events/'},        
+              {label: 'Security', to: 'https://www.apache.org/security/'},
+              {label: 'Sponsorship', to: 'https://www.apache.org/foundation/sponsorship.html'},
+              {label: 'Thanks', to: 'https://www.apache.org/foundation/thanks.html'}
+            ],
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/apache/incubator-kvrocks',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
           },
         ],
       },
@@ -93,16 +102,12 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Slack',
+                href: 'https://join.slack.com/t/kvrockscommunity/shared_invite/zt-p5928e3r-OUAK8SUgC8GOceGM6dAz6w',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'Issue Tracker',
+                href: 'https://github.com/apache/incubator-kvrocks/issues',
               },
             ],
           },
@@ -110,23 +115,28 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/apache/incubator-kvrocks',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright:  `
+        <div style="margin-top: 20px">
+                <img style="height:50px; margin-bottom: 10px; margin-top: 10px" alt="Apache Software Foundation" src= "/img/apache-incubator.svg" />
+                <p style="color: #fff;font-weight:400;text-align:left">Apache Kvrocks is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</p>
+                <div style="border-top: 1px solid #fff;min-height: 60px;line-height: 20px;text-align: center;font-family: Avenir-Medium;font-size: 14px;color: #fff;display: flex;align-items: center;"><span>Copyright ©${new Date().getFullYear()} Apache, Apache Kvrocks, Kvrocks, the Apache feather logo and the Apache Kvrocks project logo are trademarks of The Apache Software Foundation.</span></div>                
+        `,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
+
+    themes: [
+      [require.resolve("@easyops-cn/docusaurus-search-local"), { hashed: true }],
+    ]
 };
 
 module.exports = config;
