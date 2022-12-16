b2dd457daa9c461d4bb3f61a2ea60dcf8273be99
tison <wander4096@gmail.com>
2022-12-16 14:37:51 +0800

    Update links
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/supported-commands">
+            to="/docs/getting-started">
             Quick start
           </Link>
           <Link
