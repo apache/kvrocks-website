f165ab36870eae415e597fc5691dbea0f64ca5af
SongYuQiu <1594562760@qq.com>
2023-02-26 18:20:26 +0800

    correspond2review

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,17 +145,17 @@ const config = {
             ],
           },
         ],
-        // logo: {
-        //     alt: 'Apache Incubator logo',
-        //     src: 'img/apache-incubator.svg',
-        //     href: 'https://incubator.apache.org/'
-        // },
-        // copyright: `<div style="text-align: left;">
-        //   <div>
-        //     <p style="font-family: Avenir-Medium,serif;font-size: 14px;color: #999;line-height: 20px;"> Apache Kvrocks is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF. </p>
-        //   </div>
-        //   <div style="border-top: 1px solid #ccc;min-height: 60px;line-height: 20px;text-align: center;font-family: Avenir-Medium,serif;font-size: 14px;color: #999;display: flex;align-items: center;"><span>Copyright © ${new Date().getFullYear()} The Apache Software Foundation. Apache Kvrocks, Kvrocks, and its feather logo are trademarks of The Apache Software Foundation.</span></div>
-        // </div>`,
+        logo: {
+            alt: 'Apache Incubator logo',
+            src: 'img/apache-incubator.svg',
+            href: 'https://incubator.apache.org/'
+        },
+        copyright: `<div style="text-align: left;">
+          <div>
+            <p style="font-family: Avenir-Medium,serif;font-size: 14px;color: rgba(59, 61, 63, 1);line-height: 20px;"> Apache Kvrocks is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF. </p>
+          </div>
+          <div style="border-top: 1px solid rgba(59, 61, 63, 1);min-height: 60px;line-height: 20px;text-align: center;font-family: Avenir-Medium,serif;font-size: 14px;color: rgba(59, 61, 63, 1);display: flex;align-items: center;"><span>Copyright © ${new Date().getFullYear()} The Apache Software Foundation. Apache Kvrocks, Kvrocks, and its feather logo are trademarks of The Apache Software Foundation.</span></div>
+        </div>`,
       },
       prism: {
         theme: lightCodeTheme,
