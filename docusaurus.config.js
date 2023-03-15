a0e21b559c0a59ba224265983c88147600643ebc
git-hulk <hulk.website@gmail.com>
2023-03-15 10:48:13 +0800

    Support image zoom when clicking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
         editUrl: 'https://github.com/apache/incubator-kvrocks-website/tree/main/',
       },
     ],
+    [require.resolve("docusaurus-plugin-image-zoom"), {}],
   ],
 
   presets: [
@@ -157,6 +158,16 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         additionalLanguages: ['properties'],
+      },
+      zoom: {
+        selector: '.markdown :not(em) > img',
+        config: {
+            // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+            background: {
+                light: 'rgb(255, 255, 255)',
+                dark: 'rgb(50, 50, 50)'
+            }
+        }
       },
     }),
 
