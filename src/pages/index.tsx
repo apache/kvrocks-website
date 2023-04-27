d72a63bc896f1d05f7707ed0cfd3697bf1e9359f
Sandman <1559581384@qq.com>
2023-04-27 20:40:13 +0800

    update selfAdaptionV2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,53 +10,53 @@ import {resolveStaticAssetsURL} from '../cdn'
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className={clsx(styles.container)}>
-        <div className={clsx(styles.titles)}>
-          <h1 className="hero__title" style={{color:'black'}}>{siteConfig.title}</h1>
-          <h2 className='hero__secondary__title' style={{color:'rgba(40, 117, 243, 1)'}}>(incubating)</h2>
-          <div className={clsx('hero__subtitle',styles.tagline)}>{siteConfig.tagline}</div>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className={clsx(styles.container)}>
+          <div className={clsx(styles.titles)}>
+            <h1 className="hero__title" style={{color:'black'}}>{siteConfig.title}</h1>
+            <h2 className='hero__secondary__title' style={{color:'rgba(40, 117, 243, 1)'}}>(incubating)</h2>
+            <div className={clsx('hero__subtitle',styles.tagline)}>{siteConfig.tagline}</div>
+          </div>
+          <div className={clsx(styles.image)}>
+            <img src={resolveStaticAssetsURL('img/hero.png')} />
+          </div>
         </div>
-        <div className={clsx(styles.image)}>
-          <img src={resolveStaticAssetsURL('img/hero.png')} />
+        <div className={styles.buttons}>
+          <Link
+              className="button button--secondary button--lg"
+              to="/docs/getting-started">
+            QUICK START
+          </Link>
+          <Link
+              className="button button--secondary button--lg"
+              to="https://github.com/apache/incubator-kvrocks">
+            <div className='header-github-link' style={{marginRight:'10px'}}></div>
+            GITHUB
+          </Link>
+          <Link
+              className="button button--secondary button--lg"
+              to="https://kvrocks.slack.com/join/shared_invite/zt-p5928e3r-OUAK8SUgC8GOceGM6dAz6w#/shared-invite/email">
+            <div className='header-slack-link' style={{marginRight:'10px'}}></div>
+            SLACK
+          </Link>
         </div>
-      </div>
-      <div className={styles.buttons}>
-        <Link
-          className="button button--secondary button--lg"
-          to="/docs/getting-started">
-          QUICK START
-        </Link>
-        <Link
-          className="button button--secondary button--lg"
-          to="https://github.com/apache/incubator-kvrocks">
-          <div className='header-github-link' style={{marginRight:'10px'}}></div>
-          GITHUB
-        </Link>
-        <Link
-          className="button button--secondary button--lg"
-          to="https://kvrocks.slack.com/join/shared_invite/zt-p5928e3r-OUAK8SUgC8GOceGM6dAz6w#/shared-invite/email">
-          <div className='header-slack-link' style={{marginRight:'10px'}}></div>
-          SLACK
-        </Link>
-      </div>
-    </header>
+      </header>
   );
 }
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Apache Kvrocks is a distributed key value NoSQL database that uses RocksDB as storage engine and is compatible with Redis protocol.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-        <div className={styles.footerTitle}>
-          <h1>Who use Kvrocks?</h1>
-        </div>
-      </main>
-    </Layout>
+      <Layout
+          title={`${siteConfig.title}`}
+          description="Apache Kvrocks is a distributed key value NoSQL database that uses RocksDB as storage engine and is compatible with Redis protocol.">
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+          <div className={styles.footerTitle}>
+            <h1>Who use Kvrocks?</h1>
+          </div>
+        </main>
+      </Layout>
   );
 }
