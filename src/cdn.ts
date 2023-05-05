9e6b6af8b32a307a7f44953f7e792c67a5665e2d
hulk <hulk.website@gmail.com>
2023-05-05 20:21:16 +0800

    Remove the CDN path to fulfill apache website compliance (#99)

--- src/cdn.ts
+++ src/cdn.ts
@@ -3,11 +3,5 @@ export function resolveStaticAssetsURL(path: string) {
         path = '/' + path;
     }
 
-    if (process.env.NODE_ENV === 'development') {
-        return path;
-    }
-
-    // The DNS for cdn.jsdelivr.net and fastly.jsdelivr.net cannot be accessed from some regions.
-    // For now, gcore.jsdelivr.net is still accessible, but its future availability is uncertain.
-    return 'https://gcore.jsdelivr.net/gh/apache/incubator-kvrocks-website@main/static' + path;
+    return path
 }
