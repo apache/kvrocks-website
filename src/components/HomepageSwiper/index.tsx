aea74ca1b3c2e563fb70931869bcf79f85971c31
klslord <klslord@163.com>
2023-05-17 22:26:26 +0800

    remove user comment

--- src/components/HomepageSwiper/index.tsx
+++ src/components/HomepageSwiper/index.tsx
@@ -5,24 +5,6 @@ import { userData, SWIPER_ANIMATION_TIME } from "@site/src/constants";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {resolveStaticAssetsURL} from "@site/src/cdn";
-
-const userCommentsData = [
-    {
-        userName: "XXX",
-        userTitle: "XXXXXXXXXXX",
-        img: resolveStaticAssetsURL('media/users/circl-lu.png'),
-        imgTitle: 'circl.lu',
-        userComment: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    },
-    {
-        userName: "XXX",
-        userTitle: "XXXXXXXXXXX",
-        img: resolveStaticAssetsURL('media/users/huolala.png'),
-        imgTitle: 'huolala',
-        userComment: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    }
-]
 
 export default function HomepageSwiper(): JSX.Element {
     function splitUserData(userData) {
@@ -54,60 +36,10 @@ export default function HomepageSwiper(): JSX.Element {
         )
     })
 
-    const sliderItemList = userCommentsData.map((item) => {
-        return (
-            <div>
-                <div className={styles.commentsItemContainer}>
-                    <div className={styles.userContainer}>
-                        <div>
-                            <div className={styles.user}>
-                                {item.userName}
-                            </div>
-                            <div className={styles.userTitle}>
-                                {item.userTitle}
-                            </div>
-                        </div>
-                        <div>
-                            <div className={styles.userImgContainer}>
-                                <img src={item.img} srcSet={item.img} alt={item.imgTitle} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.userCommentContainer}>
-                        <div className={styles.innerUserCommentContainer}>
-                            <div className={styles.userComment}>
-                                {item.userComment}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    })
-
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false
-    }
-
     return (
         <>
             <div className={styles.swiperTitle}>
                 <h1>Who uses Kvrocks</h1>
-            </div>
-
-            <div className={styles.sliderContainer}>
-                <div>
-                    <Slider {...sliderSettings}>
-                        {sliderItemList}
-                    </Slider>
-                </div>
             </div>
 
             <div className={styles.swiperContainer}>
--- src/constants.ts
+++ src/constants.ts
@@ -1,83 +1,81 @@
-import {resolveStaticAssetsURL} from "@site/src/cdn";
-
 export const userData = [
     {
-        img: resolveStaticAssetsURL('media/users/meitu.png'),
+        img: '/media/users/meitu.png',
         title: 'Meitu',
         site: 'https://pc.meitu.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/trip.jpeg'),
+        img: '/media/users/trip.jpeg',
         title: 'CTrip',
         site: 'https://ctrip.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/jiatou.png'),
+        img: '/media/users/jiatou.png',
         title: 'Jiatou',
         site: 'https://www.jiaads.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/baidu.png'),
+        img: '/media/users/baidu.png',
         title: 'Baidu',
         site: 'https://www.baidu.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/baishancloud.png'),
+        img: '/media/users/baishancloud.png',
         title: 'BaiShanCloud',
         site: 'https://intl.baishancloud.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/rgyun.png'),
+        img: '/media/users/rgyun.png',
         title: 'Ruangao Mobile Private Marketplace',
         site: 'https://www.rgyun.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/xueqiu.png'),
+        img: '/media/users/xueqiu.png',
         title: 'Xueqiu',
         site: 'https://xueqiu.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/u-next.png'),
+        img: '/media/users/u-next.png',
         title: 'U-NEXT',
         site: 'https://unext.jp/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/circl-lu.png'),
+        img: '/media/users/circl-lu.png',
         title: 'circl.lu',
         site: 'https://circl.lu/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/datavisor.png'),
+        img: '/media/users/datavisor.png',
         title: 'datavisor',
         site: 'https://datavisor.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/huolala.png'),
+        img: '/media/users/huolala.png',
         title: 'huolala',
         site: 'https://www.huolala.cn/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/opera.png'),
+        img: '/media/users/opera.png',
         title: 'Opera',
         site: 'https://opera.com',
     },
     {
-        img: resolveStaticAssetsURL('media/users/coinindex.png'),
+        img: '/media/users/coinindex.png',
         title: 'CoinIndex',
         site: 'https://coinindex.agency/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/ahoygames.png'),
+        img: '/media/users/ahoygames.png',
         title: 'Ahoy Games',
         site: 'https://www.ahoygames.com/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/iflytek-adx.jpg'),
+        img: '/media/users/iflytek-adx.jpg',
         title: 'iflytek adx',
         site: 'https://www.voiceads.cn/',
     },
     {
-        img: resolveStaticAssetsURL('media/users/shoplazza.png'),
+        img: '/media/users/shoplazza.png',
         title: 'Shoplazza',
         site: 'https://www.shoplazza.com/',
     },
