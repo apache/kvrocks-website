35e32a62897710849d3429e026e82d509e4c9b3e
PragmaTwice <twice@apache.org>
2023-09-23 23:47:48 +0900

    add sidebar

---
+++ sidebarsDownload.js
@@ -0,0 +1,9 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+    docs: [
+        'index',
+        'community-packages',
+    ],
+};
+
+module.exports = sidebars;
