e7b636cdbd6dd3f2d36777ae6b3add6085f6f010
tison <wander4096@gmail.com>
2023-09-23 12:22:53 +0800

    drop code demo and performance charts
    Signed-off-by: tison <wander4096@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Apache Kvrocks',
-  tagline: 'A distributed key value NoSQL database that uses RocksDB as storage engine and is compatible with Redis protocol',
+  tagline: 'A distributed key value NoSQL database that uses RocksDB as storage engine and is compatible with Redis protocol.',
   url: 'https://kvrocks.apache.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
