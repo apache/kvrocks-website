4e049e130dba6f66727c8ae805841c7ba227cd90
tison <wander4096@gmail.com>
2024-03-01 16:23:41 +0800

    Add CVAT Logo (#200)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/components/UserLogos/index.tsx
+++ src/components/UserLogos/index.tsx
@@ -2,109 +2,30 @@ import React from "react"
 import styles from "./index.module.css"
 import Link from "@docusaurus/Link";
 
+// Please sort the data by title in alphabetical order.
 const userData = [
-    {
-        img: '/media/users/meitu.png',
-        title: 'Meitu',
-        site: 'https://pc.meitu.com/',
-    },
-    {
-        img: '/media/users/trip.jpeg',
-        title: 'CTrip',
-        site: 'https://ctrip.com/',
-    },
-    {
-        img: '/media/users/jiatou.png',
-        title: 'Jiatou',
-        site: 'https://www.jiaads.com/',
-    },
-    {
-        img: '/media/users/baidu.png',
-        title: 'Baidu',
-        site: 'https://www.baidu.com/',
-    },
-    {
-        img: '/media/users/baishancloud.png',
-        title: 'BaiShanCloud',
-        site: 'https://intl.baishancloud.com/',
-    },
-    {
-        img: '/media/users/rgyun.png',
-        title: 'Ruangao Mobile Private Marketplace',
-        site: 'https://www.rgyun.com/',
-    },
-    {
-        img: '/media/users/xueqiu.png',
-        title: 'Xueqiu',
-        site: 'https://xueqiu.com/',
-    },
-    {
-        img: '/media/users/u-next.png',
-        title: 'U-NEXT',
-        site: 'https://unext.jp/',
-    },
-    {
-        img: '/media/users/circl-lu.png',
-        title: 'CIRCL',
-        site: 'https://circl.lu/',
-    },
-    {
-        img: '/media/users/datavisor.png',
-        title: 'Datavisor',
-        site: 'https://datavisor.com/',
-    },
-    {
-        img: '/media/users/huolala.png',
-        title: 'Huolala',
-        site: 'https://www.huolala.cn/',
-    },
-    {
-        img: '/media/users/opera.png',
-        title: 'Opera',
-        site: 'https://opera.com',
-    },
-    {
-        img: '/media/users/coinindex.png',
-        title: 'CoinIndex',
-        site: 'https://coinindex.agency/',
-    },
-    {
-        img: '/media/users/ahoygames.png',
-        title: 'Ahoy Games',
-        site: 'https://www.ahoygames.com/',
-    },
-    {
-        img: '/media/users/iflytek-adx.jpg',
-        title: 'iFlytek ADX',
-        site: 'https://www.voiceads.cn/',
-    },
-    {
-        img: '/media/users/shoplazza.png',
-        title: 'Shoplazza',
-        site: 'https://www.shoplazza.com/',
-    },
-    {
-        img: '/media/users/netease.jpg',
-        title: 'Netease Hangzhou Institute',
-        site: 'https://research.netease.com/',
-    },
-    {
-        img: '/media/users/zto.png',
-        title: 'ZTO Express',
-        site: 'https://www.zto.com/',
-    },
-    {
-        img: '/media/users/iqiyi.png',
-        title: 'iQIYI',
-        site: 'https://www.iqiyi.com/',
-    },
-    {
-        img: '/media/users/gcore.jpg',
-        title: 'Gcore',
-        site: 'https://gcore.com/',
-    }
+    { title: 'Ahoy Games', img: '/media/users/ahoygames.png', site: 'https://www.ahoygames.com/' },
+    { title: 'BaiShanCloud', img: '/media/users/baishancloud.png', site: 'https://intl.baishancloud.com/' },
+    { title: 'Baidu', img: '/media/users/baidu.png', site: 'https://www.baidu.com/' },
+    { title: 'CIRCL', img: '/media/users/circl-lu.png', site: 'https://circl.lu/' },
+    { title: 'CTrip', img: '/media/users/trip.jpeg', site: 'https://ctrip.com/' },
+    { title: 'CVAT', img: '/media/users/cvat.png', site: 'https://www.cvat.ai/' },
+    { title: 'CoinIndex', img: '/media/users/coinindex.png', site: 'https://coinindex.agency/' },
+    { title: 'Datavisor', img: '/media/users/datavisor.png', site: 'https://datavisor.com/' },
+    { title: 'Gcore', img: '/media/users/gcore.jpg', site: 'https://gcore.com/' },
+    { title: 'Huolala', img: '/media/users/huolala.png', site: 'https://www.huolala.cn/' },
+    { title: 'Jiatou', img: '/media/users/jiatou.png', site: 'https://www.jiaads.com/' },
+    { title: 'Meitu', img: '/media/users/meitu.png', site: 'https://pc.meitu.com/' },
+    { title: 'Netease Hangzhou Institute', img: '/media/users/netease.jpg', site: 'https://research.netease.com/' },
+    { title: 'Opera', img: '/media/users/opera.png', site: 'https://opera.com' },
+    { title: 'Ruangao Mobile Private Marketplace', img: '/media/users/rgyun.png', site: 'https://www.rgyun.com/' },
+    { title: 'Shoplazza', img: '/media/users/shoplazza.png', site: 'https://www.shoplazza.com/' },
+    { title: 'U-NEXT', img: '/media/users/u-next.png', site: 'https://unext.jp/' },
+    { title: 'Xueqiu', img: '/media/users/xueqiu.png', site: 'https://xueqiu.com/' },
+    { title: 'ZTO Express', img: '/media/users/zto.png', site: 'https://www.zto.com/' },
+    { title: 'iFlytek ADX', img: '/media/users/iflytek-adx.jpg', site: 'https://www.voiceads.cn/' },
+    { title: 'iQIYI', img: '/media/users/iqiyi.png', site: 'https://www.iqiyi.com/' },
 ]
-
 
 export default function UserLogos(): JSX.Element {
     return <>
