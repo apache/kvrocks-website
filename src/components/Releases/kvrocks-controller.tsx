2e57ffb2e4279a9604fa0c99e315ef3cd9aa9c65
hulk <hulk.website@gmail.com>
2024-06-16 23:04:19 +0800

    Add the release doc for the Kvrocks controller (#227)
    * Draft release doc for the Kvrocks controller
    
    * Add the download page for Kvrocks controller

---
+++ src/components/Releases/kvrocks-controller.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const controller_versions = []
+
+type ReleaseData = {
+    name: string,
+    archive: string,
+    checksum: string,
+    signature: string,
+}
+
+function createReleaseData(version: string, vtag?: string): ReleaseData {
+    const fixedVTag = vtag ?? version;
+    const archive = `https://downloads.apache.org/kvrocks/kvrocks-controller/${version}/apache-kvrocks-controller-${fixedVTag}-src.tar.gz`
+    return {
+        name: fixedVTag,
+        archive: archive,
+        checksum: `${archive}.sha512`,
+        signature: `${archive}.asc`,
+    };
+}
+
+export default function Releases(): JSX.Element {
+    const releases = controller_versions.map(({ version, vtag }) => createReleaseData(version, vtag))
+    return <>
+        <table>
+            <thead>
+            <tr>
+                <th><b>Name</b></th>
+                <th><b>Archive</b></th>
+                <th><b>Checksum</b></th>
+                <th><b>Signature</b></th>
+            </tr>
+            </thead>
+            <tbody>
+            {releases.map(v => (
+                <tr key={v.name}>
+                    <td>{v.name}</td>
+                    <td><a href={v.archive}>tarball</a></td>
+                    <td><a href={v.checksum}>sha512</a></td>
+                    <td><a href={v.signature}>asc</a></td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    </>
+}
