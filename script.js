be41f1f2b8e43a66604c0aa347763f5970caa668
AryanSingh1112 <singharyan120505@gmail.com>
2024-09-09 18:18:01 +0530

    Add web page for documentation of bug-2229
    Created a new web page to address bug-2229. Added the following files:
    - index.html: Main HTML structure for the documentation.
    - script.js: JavaScript for interactive features.
    - style.css: CSS for styling the documentation page.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
