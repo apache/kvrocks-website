acbdaf84aec8e70b77ecc578eec65b8fffaf5215
Aryan Singh <160637316+AryanSingh1112@users.noreply.github.com>
2024-09-10 18:36:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
