e6f7299b6927837d931696dc3d15ffdd446d6beb
Twice <twice@apache.org>
2025-01-26 14:12:11 +0800

    Release 2.11.0 (#266)
    * Release 2.11.0
    
    * Update index.tsx

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const versions = [
     {
+        version: '2.11.0',
+        vtag: '2.11.0'
+    },
+    {
         version: '2.10.1',
         vtag: '2.10.1'
-	},
+    },
     {
         version: '2.10.0',
         vtag: '2.10.0'
